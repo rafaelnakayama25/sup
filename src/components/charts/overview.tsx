735ca4919e03c269ffc4f148382aa6033f116ddf
Rafael Nakayama <rafael.nakayama@facens.br>
2024-07-24 15:48:45 -0300

    fix: Correct typo in active energy card description

--- src/components/charts/overview.tsx
+++ src/components/charts/overview.tsx
@@ -626,7 +626,7 @@ export function Overview() {
           <CardHeader className="p-4 pb-0">
             <CardTitle>Active Energy</CardTitle>
             <CardDescription>
-              You're burning an average of 754 calories per day. Good job!
+              Youre burning an average of 754 calories per day. Good job!
             </CardDescription>
           </CardHeader>
           <CardContent className="flex flex-row items-baseline gap-4 p-4 pt-2">
